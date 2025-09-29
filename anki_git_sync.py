54e5697f8d77e9725ed96a652b908c2e7aa738af
Renan Saraiva dos Santos <renan.saraiva@academico.ifpb.edu.br>
2025-09-29 14:23:48 -0300

    Atualização automática Anki - 2025-09-29 14:23:47

--- anki_git_sync.py
+++ anki_git_sync.py
@@ -4,11 +4,7 @@ import datetime
 
 REPO_PATH = r"C:\Users\engre\Documents\English"
 
-commit_message = f"""Atualização automática Anki - {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-I implemented an algorithm to recognize gesture in real time
-This algorithm is easy to implement on embedding systems
-During the week, I like to work out at the gym...
-"""
+commit_message = f"""Atualização automática Anki - {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"""
 
 
 def run_cmd(cmd, cwd=None):
@@ -20,12 +16,7 @@ def run_cmd(cmd, cwd=None):
         print(result.stdout)
 
 def sync_repo():
-    print(
-    "🔄 Adicionado cartões:\n"
-    "I implemented an algorithm to recognize gesture in real time\n"
-    "This algorithm is easy to implement on embedding systems\n"
-    "During the week, I like to work out at the gym..."
-    )
+    print("🔄 Adicionado cartões:\n")
     run_cmd("git add .", cwd=REPO_PATH)
     run_cmd(f'git commit -m "{commit_message}"', cwd=REPO_PATH)
     run_cmd("git push origin main", cwd=REPO_PATH)
